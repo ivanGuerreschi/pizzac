97b5f718c33948aba43aee7c0eb8a03b339da13e
Ivan Guerreschi <ivanguerreschi86@gmail.com>
2021-11-24 20:11:14 +0100

    Hello World

---
+++ src/main.c
@@ -0,0 +1,30 @@
+/* main.c
+   Copyright (C) 2021 Ivan Guerreschi
+
+This file is part of pizzac.
+
+Author: Ivan Guerreschi <ivanguerreschi86@gmail.com>
+Maintainer: Ivan Guerreschi <ivanguerreschi86@gmail.com>
+
+pizzac is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+pizzac is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with pizzac. If not, see <http://www.gnu.org/licenses/>. */
+
+#include <stdio.h>
+#include <config.h>
+
+int
+main ()
+{
+  puts (PACKAGE_STRING ".");
+  return 0;
+}
